f83ee800c60d510a5236345819882566d6e7734b
Damien <damienroot@icloud.com>
2025-10-29 01:24:39 +0000

    refactor renderTables to prevent duplicate data

--- renderTables.js
+++ renderTables.js
@@ -1,49 +1,48 @@
-const tableBodyP2 = document.querySelector(".tableBodyP2");
-const tableBodyP3 = document.querySelector(".tableBodyP3");
-const tableBodyP4 = document.querySelector(".tableBodyP4");
-
-const td = document.createElement("td");
+const tableBody = document.querySelectorAll(".tableBody");
+const [tableBodyP2, tableBodyP3, tableBodyP4] = document.querySelectorAll(
+  ".tableBodyP2, .tableBodyP3, .tableBodyP4"
+);
 
 const renderTables = (data) => {
+  tableBody.forEach((tbody) => {
+    tbody.innerHTML = "";
+    // clear any previous data to prevent duplicates being rendered
+  });
+
   data.forEach((element) => {
     if (element[5].includes("dz-p-a")) {
       // filters out decant results
       const station = parseInt(element[5].replace("dz-p-a", ""));
 
-      const tr = document.createElement("tr");
-      const td1 = document.createElement("td");
-      const td2 = document.createElement("td");
-      const td3 = document.createElement("td");
-      const td4 = document.createElement("td");
-      const td5 = document.createElement("td");
-
       if (station < 3000) {
-        tableBodyP2.appendChild(tr);
-        renderChildren();
+        renderRow(tableBodyP2, element, station);
       }
-
       if (station > 3000 && station < 4000) {
-        tableBodyP3.appendChild(tr);
-        renderChildren();
+        renderRow(tableBodyP3, element, station);
       }
-
       if (station > 4000) {
-        tableBodyP4.appendChild(tr);
-        renderChildren();
-      }
-
-      function renderChildren() {
-        tr.appendChild(td1).textContent = element[0];
-        tr.appendChild(td2).textContent = `${element[3]}:${element[2].slice(
-          0,
-          -1
-        )}`;
-        tr.appendChild(td3).textContent = null;
-        tr.appendChild(td4).textContent = null;
-        tr.appendChild(td5).textContent = station;
+        renderRow(tableBodyP4, element, station);
       }
     }
   });
 };
+
+function renderRow(tableBody, element, station) {
+  const tr = document.createElement("tr");
+  const td1 = document.createElement("td");
+  const td2 = document.createElement("td");
+  const td3 = document.createElement("td");
+  const td4 = document.createElement("td");
+  const td5 = document.createElement("td");
+
+  td1.textContent = element[0];
+  td2.textContent = `${element[3]}:${element[2].slice(0, -1)}`;
+  td3.textContent = "";
+  td4.textContent = "";
+  td5.textContent = station;
+
+  tr.append(td1, td2, td3, td4, td5);
+  tableBody.appendChild(tr);
+}
 
 export default renderTables;
